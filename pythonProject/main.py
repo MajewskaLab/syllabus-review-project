de656236f2a4cb8f29731d73aa0a0919fe941f25
ak24289 <126099229+ak24289@users.noreply.github.com>
2023-04-10 15:12:41 -0400

    Updated loop

---
+++ pythonProject/main.py
@@ -0,0 +1,20 @@
+import os
+from textblob import TextBlob
+
+# Set directory path
+directory = r'C:\Syllabus Review Project\syllabus-review-project\attendance'
+
+# Loop through files in directory
+for filename in os.listdir(directory):
+    # Check if file is a text file
+    if filename.endswith('.txt'):
+        # Open file and read its contents
+        with open(os.path.join(directory, filename), 'r') as file:
+            contents = file.read()
+            # Create TextBlob object
+            blob = TextBlob(contents)
+            # Print sentiment and objectivity scores
+            print(f"Sentiment for {filename}: {blob.sentiment.polarity}")
+            print(f"Objectivity for {filename}: {1-blob.sentiment.subjectivity}")
+
+
